500124628bfb4dd9db5cdb3d89057ecec0a7201c
Dmitriy Rokachov <rokachov@gmail.com>
2016-12-29 14:55:13 +0200

    29/12/2016

---
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level22.lesson18.big01;
+
+/**
+ * Created by Rumata on 29.12.2016.
+ */
+public class Figure
+{
+}
---
+++ src/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level22.lesson18.big01;
+
+/**
+ * Created by Rumata on 29.12.2016.
+ */
+public class Tetris
+{
+    public Field field;
+    public Figure figure;
+    public Tetris game;
+
+    public Field getField()
+    {
+        return field;
+    }
+
+    public Figure getFigure()
+    {
+        return figure;
+    }
+    public void run() {}
+
+    public void step(){}
+
+
+
+    public static void main(String[] args)
+    {
+
+
+    }
+}
