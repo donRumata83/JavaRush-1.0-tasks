f3233c71f6ca8699d2cb0509d8993c820d2e7ae6
Dmitriy Rokachov <rokachov@gmail.com>
2017-01-05 11:10:51 +0200

    05012017

--- src/com/javarush/test/level22/lesson18/big01/Field.java
+++ src/com/javarush/test/level22/lesson18/big01/Field.java
@@ -1,19 +1,24 @@
 package com.javarush.test.level22.lesson18.big01;
 
+import java.util.ArrayList;
+
 /**
- * Created by Rumata on 29.12.2016.
+ * Класс Field описывает "поле клеток" игры Тетрис
  */
 public class Field
 {
+    //ширина и высота
     private int width;
     private int height;
+
+    //матрица поля: 1 - клетка занята, 0 - свободна
     private int[][] matrix;
 
     public Field(int width, int height)
     {
         this.width = width;
         this.height = height;
-        this.matrix = new int[height][width];
+        matrix = new int[height][width];
     }
 
     public int getWidth()
@@ -31,36 +36,94 @@ public class Field
         return matrix;
     }
 
+    /**
+     * Метод возвращает значение, которое содержится в матрице с координатами (x,y)
+     * Если координаты за пределами матрицы, метод возвращает null.
+     */
+    public Integer getValue(int x, int y)
+    {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            return matrix[y][x];
+
+        return null;
+    }
+
+    /**
+     *  Метод устанавливает переданное значение(value) в ячейку матрицы с координатами (x,y)
+     */
+    public void setValue(int x, int y, int value)
+    {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            matrix[y][x] = value;
+    }
+
+    /**
+     * Метод печатает на экран текущее содержание матрицы
+     */
     public void print()
     {
-        for (int i = 0; i < getHeight(); i++)
+        //Создаем массив, куда будем "рисовать" текущее состояние игры
+        int[][] canvas = new int[height][width];
+
+        //Копируем "матрицу поля" в массив
+        for (int i = 0; i < height; i++)
         {
-            for (int j = 0; j < getWidth(); j++)
+            for (int j = 0; j < width; j++)
             {
-                if (getValue(j, i) == 0) System.out.print(".");
-                else System.out.print("X");
+                canvas[i][j] = matrix[i][j];
+            }
+        }
+
+        //Копируем фигурку в массив, только непустые клетки
+        int left = Tetris.game.getFigure().getX();
+        int top = Tetris.game.getFigure().getY();
+        int[][] brickMatrix = Tetris.game.getFigure().getMatrix();
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (top + i >= height || left + j >= width) continue;
+                if (brickMatrix[i][j] == 1)
+                    canvas[top + i][left + j] = 2;
+            }
+        }
+
+
+        //Выводим "нарисованное" на экран, но начинаем с "границы кадра".
+        System.out.println("---------------------------------------------------------------------------\n");
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int index = canvas[i][j];
+                if (index == 0)
+                    System.out.print(" . ");
+                else if (index == 1)
+                    System.out.print(" X ");
+                else if (index == 2)
+                    System.out.print(" X ");
+                else
+                    System.out.print("???");
             }
             System.out.println();
         }
+
+
+        System.out.println();
+        System.out.println();
     }
 
+    /**
+     * Удаляем заполненные линии
+     */
     public void removeFullLines()
     {
-    }
-
-    public Integer getValue(int x, int y)
-    {
-        if (x < 0 || y < 0 || y > getHeight() - 1 || x > getWidth() - 1)
-        {
-            return -1;
-        } else
-            return matrix[y][x];
-    }
-
-    public void setValue(int x, int y, int value)
-    {
-        if (x < 0 || y < 0 || y > getHeight() - 1 || x > getWidth() - 1)
-        {
-        } else matrix[y][x] = value;
+        //Например так:
+        //Создаем список для хранения линий
+        //Копируем все непустые линии в список.
+        //Добавляем недостающие строки в начало списка.
+        //Преобразуем список обратно в матрицу
     }
 }
