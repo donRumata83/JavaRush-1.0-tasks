6a7513fd474740e6e1ea4518126d94fd4b0f8f6d
Dmitriy Rokachov <rokachov@gmail.com>
2017-01-11 18:43:18 +0200

    11012017

--- src/com/javarush/test/level23/lesson06/task02/Solution.java
+++ src/com/javarush/test/level23/lesson06/task02/Solution.java
@@ -7,45 +7,48 @@ package com.javarush.test.level23.lesson06.task02;
 public class Solution {
     public final class Constants
     {
-
+        public static final String snae = "Server is not accessible for now.";
+        public static final String uue = "User is not authorized.";
+        public static final String bue = "User is banned.";
+        public static final String aie = "Access is denied.";
     }
     public class ServerNotAccessibleException extends Exception {
         public ServerNotAccessibleException() {
-            super("Server is not accessible for now.");
+            super(Constants.snae);
         }
 
         public ServerNotAccessibleException(Throwable cause) {
-            super("Server is not accessible for now.", cause);
+            super(Constants.snae, cause);
         }
     }
 
     public class UnauthorizedUserException extends Exception {
         public UnauthorizedUserException() {
-            super("User is not authorized.");
+            super(Constants.uue);
         }
 
         public UnauthorizedUserException(Throwable cause) {
-            super("User is not authorized.", cause);
+            super(Constants.uue, cause);
         }
     }
 
     public class BannedUserException extends Exception {
         public BannedUserException() {
-            super("User is banned.");
+            super(Constants.bue);
         }
 
         public BannedUserException(Throwable cause) {
-            super("User is banned.", cause);
+            super(Constants.bue, cause);
         }
     }
 
     public class RestrictionException extends Exception {
         public RestrictionException() {
-            super("Access is denied.");
+            super(Constants.aie);
         }
 
         public RestrictionException(Throwable cause) {
-            super("Access is denied.", cause);
+            super(Constants.aie, cause);
         }
     }
 }
