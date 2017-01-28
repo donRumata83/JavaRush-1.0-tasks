0b34757f6cff335218419de3bca43bc3b71681a4
Dmitriy Rokachov <rokachov@gmail.com>
2017-01-28 12:46:55 +0200

    28/01/2017

--- src/com/javarush/test/level25/lesson05/task01/Solution.java
+++ src/com/javarush/test/level25/lesson05/task01/Solution.java
@@ -8,23 +8,31 @@ package com.javarush.test.level25.lesson05.task01;
 4. Если нить прекратила работу, то выведите в консоль ее приоритет.
 Используйте switch.
 */
-public class Solution {
-    public static void processThreads(Thread... threads) {
-       for (Thread thread : threads)
-       {
-           switch (thread.getState())
-           {
-               case NEW: thread.start();
-               break;
-               case WAITING: thread.interrupt();
-               break;
-               case RUNNABLE: thread.isInterrupted();
-               break;
-               case TERMINATED:
-                   System.out.println(thread.getPriority());
-               break;
+public class Solution
+{
+    public static void processThreads(Thread... threads)
+    {
+        for (Thread thread : threads)
+        {
+            switch (thread.getState())
+            {
+                case NEW:
+                    thread.start();
+                    break;
+                case WAITING:
+                case TIMED_WAITING:
+                case BLOCKED:
+                    thread.interrupt();
+                    break;
+                case RUNNABLE:
+                    thread.isInterrupted();
+                    break;
+                case TERMINATED:
+                    System.out.println(thread.getPriority());
+                    break;
 
-           }
-       }
+
+            }
+        }
     }
 }
